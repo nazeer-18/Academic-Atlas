59bc5517c2b433a572a1afa280b9c318df3a597c
SHAIK NAZEER <ch.en.u4cse21161@ch.students.amrita.edu>
2024-06-20 16:57:11 +0530

    integrated googleapis to add resources into g drive api

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,16 +3,16 @@ const User = require('../models/user');
 const authRoute = express.Router();
 
 //login route
-authRoute.post('/login',async(req,res)=>{
-    const {email,password} = req.body;
-    const user = await User.findOne({email: email});
-    if(!user){
+authRoute.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) {
         return res.status(400).json({
             success: false,
             message: 'User not found'
         })
     }
-    if(user.password !== password){
+    if (user.password !== password) {
         return res.status(400).json({
             success: false,
             message: 'Invalid password'
@@ -25,30 +25,30 @@ authRoute.post('/login',async(req,res)=>{
 });
 
 //register route
-authRoute.post('/register',async(req,res)=>{
-    const {userName,email,password,mobile} = req.body;
-    console.log(userName,email,password,mobile)
-    const user = await User.findOne({email: email})
-    if(user){
+authRoute.post('/register', async (req, res) => {
+    const { userName, email, password, mobile } = req.body;
+    console.log(userName, email, password, mobile)
+    const user = await User.findOne({ email: email })
+    if (user) {
         return res.status(400).json({
             success: false,
             message: 'User already exists'
         })
     }
     const newUser = new User({
-        userName : userName,
-        email : email,
-        password : password,
-        mobile : mobile
+        userName: userName,
+        email: email,
+        password: password,
+        mobile: mobile
     })
-    try{
+    try {
         newUser.save()
         return res.status(200).json({
             success: true,
             message: 'User registered successfully'
-        })  
+        })
     }
-    catch(err){
+    catch (err) {
         return res.status(400).json({
             success: false,
             message: 'User not registered'
