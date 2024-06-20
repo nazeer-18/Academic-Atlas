77f785936321af4ac28493e9f367966808f7ebec
SHAIK NAZEER <ch.en.u4cse21161@ch.students.amrita.edu>
2024-06-20 16:20:44 +0530

    updated contribute.js

--- academic-atlas/src/components/Contribute.js
+++ academic-atlas/src/components/Contribute.js
@@ -1,5 +1,5 @@
-import React,{useState} from 'react'
-import resourceService from '../services/resourceService'
+import React, { useState } from 'react';
+import resourceService from '../services/resourceService';
 
 export default function Contribute() {
     const [data, setData] = useState({
@@ -11,82 +11,111 @@ export default function Contribute() {
         course: '',
         Link: '',
         Description: '',
-        pdfFile: ''
-    })
-    const handleSubmit = async () => {
-        console.log(data)
-        const response = await resourceService.addExam(data.category, data.academicYear, data.branch, data.course, data.pdfFile, data.author)
-            .then(res => {
-                if (res.data.success) {
-                    alert(res.data.message);
-                }
-                else {
-                    alert(res.data.message);
-                }
-            }).catch(err => {
-                alert(err);
-            })
-    }
+        pdfFile: null,
+    });
+
+    const handleFileChange = (e) => {
+        setData({ ...data, pdfFile: e.target.files[0] });
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setData({ ...data, [name]: value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append('title', data.title);
+        formData.append('category', data.category);
+        formData.append('author', data.author);
+        formData.append('academicYear', data.academicYear);
+        formData.append('branch', data.branch);
+        formData.append('course', data.course);
+        formData.append('Link', data.Link);
+        formData.append('Description', data.Description);
+        formData.append('pdfFile', data.pdfFile);
+
+        try {
+            const res = await resourceService.addExam(formData);
+            if (res.data.success) {
+                alert(res.data.message);
+            } else {
+                alert(res.data.message);
+            }
+        } catch (err) {
+            alert(err.message);
+        }
+    };
+
     return (
         <div>
-            <h1>Contribute</h1> 
-            <input 
-                type="text" 
-                placeholder="Title" 
-                value={data.title}
-                onChange={(e) => setData({...data, title: e.target.value})}
-            /> <br />
-            <input
-                type="text"
-                placeholder="Author"
-                value={data.author}
-                onChange={(e) => setData({...data, author: e.target.value})}
-            /> <br />
-            <input
-                type="text"
-                placeholder="Year"
-                value={data.academicYear}
-                onChange={(e) => setData({...data, academicYear: e.target.value})}
-            /> <br />
-            <input
-                type="text"
-                placeholder="Link"
-                value={data.Link}
-                onChange={(e) => setData({...data, Link: e.target.value})}
-            /> <br />
-            <input
-                type="text"
-                placeholder="Description"
-                value={data.Description}
-                onChange={(e) => setData({...data, Description: e.target.value})}
-            /> <br />
-            <input
-                type="file"
-                placeholder="PDF file"
-                value={data.pdfFile}
-                onChange={(e) => setData({...data, pdfFile: e.target.value})}
-            /> <br />
-            <select
-                value={data.category}
-                onChange={(e) => setData({...data, category: e.target.value})}
-            >
-                <option value="exam">Exam</option>
-                <option value="capstone">Capstone</option>
-            </select> <br />
-            <input
-                type="text"
-                placeholder="Branch"
-                value={data.branch}
-                onChange={(e) => setData({...data, branch: e.target.value})}
-            /> <br />
-            <input
-                type="text"
-                placeholder="Course"
-                value={data.course}
-                onChange={(e) => setData({...data, course: e.target.value})}
-            /> <br />
-            
-            <button onClick={handleSubmit}>Submit</button>
+            <h1>Contribute</h1>
+            <form onSubmit={handleSubmit} encType="multipart/form-data">
+                <input
+                    type="text"
+                    name="title"
+                    placeholder="Title"
+                    value={data.title}
+                    onChange={handleChange}
+                /> <br />
+                <input
+                    type="text"
+                    name="author"
+                    placeholder="Author"
+                    value={data.author}
+                    onChange={handleChange}
+                /> <br />
+                <input
+                    type="text"
+                    name="academicYear"
+                    placeholder="Year"
+                    value={data.academicYear}
+                    onChange={handleChange}
+                /> <br />
+                <input
+                    type="text"
+                    name="Link"
+                    placeholder="Link"
+                    value={data.Link}
+                    onChange={handleChange}
+                /> <br />
+                <input
+                    type="text"
+                    name="Description"
+                    placeholder="Description"
+                    value={data.Description}
+                    onChange={handleChange}
+                /> <br />
+                <input
+                    type="file"
+                    accept="application/pdf"
+                    onChange={handleFileChange}
+                /> <br />
+                <select
+                    name="category"
+                    value={data.category}
+                    onChange={handleChange}
+                >
+                    <option value="exam">Exam</option>
+                    <option value="capstone">Capstone</option>
+                </select> <br />
+                <input
+                    type="text"
+                    name="branch"
+                    placeholder="Branch"
+                    value={data.branch}
+                    onChange={handleChange}
+                /> <br />
+                <input
+                    type="text"
+                    name="course"
+                    placeholder="Course"
+                    value={data.course}
+                    onChange={handleChange}
+                /> <br />
+                <button type="submit">Submit</button>
+            </form>
         </div>
-    )
+    );
 }
