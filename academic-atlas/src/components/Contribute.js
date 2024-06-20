1746f6bd4153462712792457366aa263149a2563
SHAIK NAZEER <ch.en.u4cse21161@ch.students.amrita.edu>
2024-06-20 21:09:16 +0530

    modified contribute paeg

--- academic-atlas/src/components/Contribute.js
+++ academic-atlas/src/components/Contribute.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import resourceService from '../services/resourceService';
 import '../styles/ContributePage.css';
-import { useLocation } from 'react-router-dom'
 
 export default function Contribute() {
     const [data, setData] = useState({
@@ -37,7 +36,9 @@ export default function Contribute() {
         formData.append('Link', data.Link);
         formData.append('Description', data.Description);
         formData.append('pdfFile', data.pdfFile);
-
+        for (let key of formData.keys()) {
+            console.log(key, formData.get(key));
+        }
         try {
             const res = await resourceService.addExam(formData);
             if (res.data.success) {
@@ -51,92 +52,116 @@ export default function Contribute() {
     };
 
     return (
-        <div className = "contributepage">
-        <div class="contribute-container">
-            <div class="contribute-header">
-                Contribute Resource
+        <div className="contributepage">
+            <div className="contribute-container">
+                <div className="contribute-header">
+                    Contribute Resource
+                </div>
+                <div className="contribute-content">
+                    <label className='contributequestion' for="typefields">What would you like to contribute today ?</label>
+                    <select id="contribution" name="category"
+                        value={data.category}
+                        onChange={handleChange}
+                    >
+                        <option value="">Select Type</option>
+                        <option value="capstone">Projects</option>
+                        <option value="capstone">Research Papers</option>
+                        <option value="exam">Mid/End sem papers</option>
+                    </select>
+                </div>
+
+                <form onSubmit={handleSubmit} encType="multipart/form-data">
+
+                    {
+                        data && data.category === 'capstone' &&
+                        <>
+                            <label className="contributefields" htmlFor="title">Author: </label>
+                            <input
+                                type="text"
+                                name="author"
+                                placeholder="Enter author of your research paper"
+                                value={data.author}
+                                onChange={handleChange}
+                            /> <br />
+                            <label className="resultfields" htmlFor="title">Title: </label>
+                            <input
+                                type="text"
+                                name="title"
+                                placeholder="Enter title of your project"
+                                value={data.title}
+                                onChange={handleChange}
+                            /> <br />
+
+                            <label className="contributefields" htmlFor="title">Description: </label>
+                            <input
+                                type="text"
+                                name="Description"
+                                placeholder="Enter Description of your project"
+                                value={data.Description}
+                                onChange={handleChange}
+                            /> <br />
+
+                            <label className="contributefields" htmlFor="title">Link: </label>
+                            <input
+                                type="text"
+                                name="Link"
+                                placeholder="Paste the github link of your project"
+                                value={data.Link}
+                                onChange={handleChange}
+                            /> <br />
+                        </>
+                    }
+
+                    {
+                        data && data.category !== '' &&
+                        <>
+
+                            <label className="contributefields" htmlFor="title">Author: </label>
+                            <input 
+                                type="text"
+                                name="author"
+                                placeholder="Enter author of your research paper"
+                                value={data.author}
+                                onChange={handleChange}
+                            /> <br />
+
+                            <label className="contributefields" htmlFor="title">Year: </label>
+                            <input
+                                type="text"
+                                name="academicYear"
+                                placeholder="Enter in which year project is done"
+                                value={data.academicYear}
+                                onChange={handleChange}
+                            /> <br />
+
+                            <label className="contributefields" htmlFor="title">Branch: </label>
+                            <input
+                                type="text"
+                                name="branch"
+                                placeholder="Enter your department"
+                                value={data.branch}
+                                onChange={handleChange}
+                            /> <br />
+
+                            <label className="contributefields" htmlFor="title">Courses: </label>
+                            <input
+                                type="text"
+                                name="course"
+                                placeholder="Enter the courses related to your project"
+                                value={data.course}
+                                onChange={handleChange}
+                            /> <br />
+
+                            <input
+                                type="file"
+                                accept="application/pdf"
+                                onChange={handleFileChange}
+                            /> <br />
+                            <button type="submit">Submit</button>
+                        </>
+                    }
+                </form>
             </div>
-            <div class="contribute-content">
-                <label className='contributequestion' for="typefields">What would you like to contribute today ?</label>
-                <select id="contribution" 
-                value={data.category}
-                onChange={handleChange}
-                >
-                    <option value="" disabled selected>Select Type</option>
-                                        
-                    <option value="project">Projects</option>
-                    <option value="article">Research Papers</option>
-                    <option value="code">Mid/End sem papers</option>
-                </select>
-               
-            </div>
-            <form onSubmit={handleSubmit} encType="multipart/form-data">
-            <label className="resultfields" htmlFor="title">Title: </label>
-                <input
-                    type="text"
-                    name="title"
-                    placeholder="Enter title of your project"
-                    value={data.title}
-                    onChange={handleChange}
-                /> <br/>
-                <label className="contributefields" htmlFor="title">Author: </label>
-                <input
-                    type="text"
-                    name="author"
-                    placeholder="Enter author of your research paper"
-                    value={data.author}
-                    onChange={handleChange}
-                /> <br />
-                <label className="contributefields" htmlFor="title">Year: </label>
-                <input
-                    type="text"
-                    name="academicYear"
-                    placeholder="Enter in which year project is done"
-                    value={data.academicYear}
-                    onChange={handleChange}
-                /> <br />
-                <label className="contributefields" htmlFor="title">Link: </label>
-                <input
-                    type="text"
-                    name="Link"
-                    placeholder="Paste the github link of your project"
-                    value={data.Link}
-                    onChange={handleChange}
-                /> <br />
-                <label className="contributefields" htmlFor="title">Description: </label>
-                <input
-                    type="text"
-                    name="Description"
-                    placeholder="Enter Description of your project"
-                    value={data.Description}
-                    onChange={handleChange}
-                /> <br />
-                <label className="contributefields" htmlFor="title">Branch: </label>
-                <input
-                    type="text"
-                    name="branch"
-                    placeholder="Enter your department"
-                    value={data.branch}
-                    onChange={handleChange}
-                /> <br />
-                <label className="contributefields" htmlFor="title">Courses: </label>
-                <input
-                    type="text"
-                    name="course"
-                    placeholder="Enter the courses related to your project"
-                    value={data.course}
-                    onChange={handleChange}
-                /> <br />
-                {/* <input
-                    type="file"
-                    accept="application/pdf"
-                    onChange={handleFileChange}
-                /> <br /> */}
-               
-                
-                <button type="submit">Submit</button>
-            </form>
         </div>
-        </div>    
     );
 }
