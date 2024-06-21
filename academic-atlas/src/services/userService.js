d920f96cfdc1b864382424319fc5ad21d8ba01ec
SHAIK NAZEER <ch.en.u4cse21161@ch.students.amrita.edu>
2024-06-22 01:31:38 +0530

    modified css

--- academic-atlas/src/services/userService.js
+++ academic-atlas/src/services/userService.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 const server_url = 'https://academic-atlas-server.onrender.com';
 
 class userService {
-    login(data){
-        return axios.post(server_url + '/api/auth/login',data);
+    login(data) {
+        return axios.post(server_url + '/api/auth/login', data);
     }
-    resetpwd(email,password){ 
-        return axios.post(server_url + '/api/auth/resetpwd',{email,password});
+    resetpwd(email, password) {
+        return axios.post(server_url + '/api/auth/resetpwd', { email, password });
     }
 }
 
