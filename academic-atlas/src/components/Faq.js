9eaaea1bc106a60578172ffd40b50bd3d6e320fc
Rakshith <arakshith3108@gmail.com>
2024-06-23 23:49:02 +0530

    Developed Curriculum Page and modified Css for footer components

---
+++ academic-atlas/src/components/Faq.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Faq.css';
+
+export default function Faq() {
+    return (
+        <div className="Faq-page">
+            <h1>Frequently Asked Questions</h1>
+            
+            
+        </div>
+    );
+}
